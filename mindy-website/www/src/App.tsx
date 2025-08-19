16fb0091426e74e2490732434ed585fafe3043df
object-Object <object@objectobject.ca>
2025-08-19 00:11:00 -0400

    Add footer with source link

--- mindy-website/www/src/App.tsx
+++ mindy-website/www/src/App.tsx
@@ -1,4 +1,11 @@
-import { Center, Loader, MantineProvider } from "@mantine/core";
+import {
+    AppShell,
+    Center,
+    Flex,
+    Loader,
+    MantineProvider,
+    Text,
+} from "@mantine/core";
 import "@mantine/core/styles.css";
 import { ReactFlowProvider } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
@@ -12,15 +19,39 @@ import { theme } from "./theme";
 export default function App() {
     return (
         <MantineProvider theme={theme} defaultColorScheme="dark">
-            <Center h="100vh">
-                <Suspense fallback={<Loader color="indigo" size="lg" />}>
-                    <LogicVMProvider>
-                        <ReactFlowProvider>
-                            <LogicVMFlow />
-                        </ReactFlowProvider>
-                    </LogicVMProvider>
+            <ReactFlowProvider>
+                <Suspense
+                    fallback={
+                        <Center pt="50vh">
+                            <Loader color="indigo" size="lg" />
+                        </Center>
+                    }
+                >
+                    <AppShell h="100vh" footer={{ height: 30 }}>
+                        <AppShell.Main h="100%">
+                            <LogicVMProvider>
+                                <LogicVMFlow />
+                            </LogicVMProvider>
+                        </AppShell.Main>
+
+                        <AppShell.Footer>
+                            <Flex h="100%" justify="center" align="center">
+                                <Text
+                                    size="sm"
+                                    c="dimmed"
+                                    td="underline"
+                                    component="a"
+                                    href="https://github.com/object-Object/mindy/tree/main/mindy-website"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Source
+                                </Text>
+                            </Flex>
+                        </AppShell.Footer>
+                    </AppShell>
                 </Suspense>
-            </Center>
+            </ReactFlowProvider>
         </MantineProvider>
     );
 }
